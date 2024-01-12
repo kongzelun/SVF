a6619ce34af22ad8697d7f35ceeeac9623940769
KZL <lz3450@outlook.com>
2024-01-11 19:44:57 -0600

    clean remove LLVMUtil::llvmToString() calls

--- svf-llvm/lib/LLVMModule.cpp
+++ svf-llvm/lib/LLVMModule.cpp
@@ -278,26 +278,6 @@ void LLVMModuleSet::createSVFFunction(const Function* func)
                                        SVFUtil::isa<ReturnInst>(inst));
             }
 
-            // Set instruction's string representation
-            // if (inst.hasName() && !inst.getName().empty())
-            // {
-            //     svfInst->setName(inst.getName().str());
-            // }
-            // else
-            // {
-            //     std::string str = LLVMUtil::llvmToString(inst);
-            //     auto it = str.begin(), ite = str.end();
-            //     while (it != ite && std::isspace(*it))
-            //         ++it;
-            //     // 0xf (15) is the max length a local string can hold
-            //     svfInst->setName({it, std::min(it + 0xf, ite)});
-            // }
-            std::string str = LLVMUtil::llvmToString(inst);
-            auto it = str.begin(), ite = str.end();
-            while (it != ite && std::isspace(*it))
-                ++it;
-            svfInst->setName({it, ite});
-
             svfBB->addInstruction(svfInst);
             addInstructionMap(&inst, svfInst);
         }
